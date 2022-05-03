62ee7d914b199a0b77dadb81d7ba57fcd3a14fff
nurmdrafi <nurmdrafi2014@gmail.com>
2022-05-03 22:21:57 +0600

    get home page items

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -6,11 +6,11 @@ const Banner = () => {
     <div className="banner-container">
       <img src={BannerImg} alt="" />
       <div className="position-absolute w-100 z-index-1 bottom-0">
-      <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 40" preserveAspectRatio="none" shape-rendering="auto">
+      <svg className="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 40" preserveAspectRatio="none" shapeRendering="auto">
         <defs>
           <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"></path>
         </defs>
-        <g class="moving-waves">
+        <g className="moving-waves">
           <use xlinkHref="#gentle-wave" x="48" y="-1" fill="rgba(251,251,251,0.40"></use>
           <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(251,251,251,0.35)"></use>
           <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(251,251,251,0.25)"></use>
--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import Banner from '../Banner/Banner';
+import React, { useEffect, useState } from "react";
+import Banner from "../Banner/Banner";
+import './Home.css'
+
+const axios = require("axios").default;
 
 const Home = () => {
-    return (
-        <div>
-            <Banner></Banner>
-        </div>
-    );
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const getItems = async () => {
+      const url = "http://localhost:5000/inventory";
+      const { data } = await axios.get(url);
+      setItems(data);
+    };
+    getItems();
+  }, []);
+
+  return (
+    <div>
+      <Banner></Banner>
+      {/* Item section */}
+      <section className="container">
+      
+          <div className="row row-cols-1 row-cols-lg-3 g-0">
+              {
+                  items.slice(0,6).map((item, idx) => <div className="col" keys={idx}>
+                      <div className="items">
+                      <h3>{item.name}</h3>
+                      <h2>{item.brand}</h2>
+                      <div className="image-container">
+                          <img src={item.image} alt="" />
+                      </div>
+                      </div>
+                  </div>)
+              }
+          </div>
+          </section>
+      
+    </div>
+  );
 };
 
 export default Home;
