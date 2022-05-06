3ca31a00ff939e3b450a8d5415766a0ad791115e
nurmdrafi <nurmdrafi2014@gmail.com>
2022-05-07 02:15:14 +0600

    implement reset password

--- src/components/Auth/LogIn/LogIn.js
+++ src/components/Auth/LogIn/LogIn.js
@@ -1,13 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import "./Login.css";
 import { FcGoogle } from "react-icons/fc";
-import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import {
+  useSendPasswordResetEmail,
+  useSignInWithEmailAndPassword,
+} from "react-firebase-hooks/auth";
 import auth from "../../../firebase.init";
 import toast, { Toaster } from "react-hot-toast";
+import Modal from "react-modal";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
 
 const LogIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const {
     register,
     handleSubmit,
@@ -15,11 +33,17 @@ const LogIn = () => {
     setError,
     reset,
   } = useForm();
+  // Sign in with email and password
   const [signInWithEmailAndPassword, user, loading, error] =
     useSignInWithEmailAndPassword(auth);
+  // Send password reset email
+  const [sendPasswordResetEmail, sendingReset, errorReset] =
+    useSendPasswordResetEmail(auth);
   const onSubmit = (data) => {
+    setEmail(data.email);
+    setPassword(data.password);
     // Email Validation
-    if (!/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(data.email)) {
+    if (!/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
       setError("email", {
         type: "validate",
         message: "Please provide a valid email.",
@@ -27,9 +51,32 @@ const LogIn = () => {
     }
 
     // Sign in with email and password
-    signInWithEmailAndPassword(data.email, data.password);
+    signInWithEmailAndPassword(email, password);
     // reset input field
     reset();
+  };
+
+  // Forget password & Modal
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  const handleForgetPassword = (e) => {
+    e.preventDefault();
+    const email = e.target.forgetEmail.value;
+    if (!email) {
+      toast.error("Please enter your email");
+    } else {
+      sendPasswordResetEmail(email);
+      toast.success("Email Send, Please check your email box.");
+      e.target.reset();
+    }
   };
 
   // Toast Notification
@@ -40,13 +87,21 @@ const LogIn = () => {
   }, [user]);
 
   useEffect(() => {
-    if (error){
-      toast.error(error.message, {
-        id: "email/password error"
-      })
+    if(errorReset){
+      toast.error(error?.message, {
+        id: "errorReset",
+      });
     }
-  }, [error])
-  if (loading) {
+  }, [errorReset]);
+  useEffect(() => {
+    if (error) {
+      toast.error(error?.message, {
+        id: "email/password error",
+      });
+    }
+  }, [error]);
+
+  if (loading || sendingReset) {
     return (
       <div className="d-flex justify-content-center my-5 py-5">
         <div className="spinner-border" role="status">
@@ -55,8 +110,7 @@ const LogIn = () => {
       </div>
     );
   }
-  
-  
+
   return (
     <div className="form-container">
       <div>
@@ -92,14 +146,21 @@ const LogIn = () => {
             className={`form-control ${errors.password ? "is-invalid" : ""}`}
           />
           {/* Error Message */}
-          <p className="text-warning">
+          <p className="text-danger">
             {errors.password?.type === "required" && "Password is required"}
           </p>
-          <p className="text-muted">Your password should contain at least one uppercase, one lowercase, one numeric, one special character and minimum 8 characters.</p>
+          <p className="text-muted">
+            Your password should contain at least one uppercase, one lowercase,
+            one numeric, one special character and minimum 8 characters.
+          </p>
         </div>
-        <Link className="text-end" to="/">
+        <p
+          role="button"
+          className="text-end text-primary text-decoration-underline"
+          onClick={openModal}
+        >
           Forget Password?
-        </Link>
+        </p>
         <input type="submit" value="Continue" />
       </form>
       {/* Divider */}
@@ -126,6 +187,38 @@ const LogIn = () => {
       <p className="text-center">
         Not a member? <Link to="/register">Register now</Link>
       </p>
+      {/* Forget Password */}
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+        ariaHideApp={false}
+      >
+        <button
+          onClick={closeModal}
+          className="btn btn-close ms-auto d-block"
+        ></button>
+        <h2 className="text-center">Trouble Logging In?</h2>
+        <form onSubmit={handleForgetPassword}>
+          <p className="text-muted">
+            Enter your email and we'll send you link to get back into your
+            account.
+          </p>
+          <div className="field">
+            <input
+              type="email"
+              name="forgetEmail"
+              className="p-1 mb-2 border border-2"
+            />
+            <input
+              type="submit"
+              value="Reset Password"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
+      </Modal>
     </div>
   );
 };
