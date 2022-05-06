9aa350c4d919789eb746cfdf9f98fb4e9506571e
nurmdrafi <nurmdrafi2014@gmail.com>
2022-05-07 00:48:29 +0600

    not found page added

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NotFound.css";
 
 const NotFound = () => {
-    return (
-        <div>
-        </div>
-    );
+  return (
+    <div className="not-found">
+      <div className="details">
+        <p>ERROR 404</p>
+        <h2>
+          Oops! The page you're <br /> looking for is'nt here.
+        </h2>
+        <p>
+          You might have the wrong address, or the page <br />
+          may have moved.
+        </p>
+        <Link to="/home">
+          <button type="button" className="btn btn-dark">
+            Back to homepage
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
